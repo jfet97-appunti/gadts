ad1ccfc54122645d455183607e0fb6d75481bd96
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-10-21 16:53:38 +0200

    wtf

--- examples/expr.ts
+++ examples/expr.ts
@@ -22,7 +22,7 @@ type Expr<A> =
 // --------------------------- Num<A> ---------------------------
 // (Functor Expr) => Expr number ~ (forall r. (number -> r) -> Expr r)
 
-type Num<A> = {
+interface Num<A> {
     value: number
     eq: (_: number) => A
     _tag: "Num"
@@ -40,7 +40,7 @@ function Num_y(n: number) {
 // -------------------------- Bool<A> --------------------------
 // (Functor Expr) => Expr boolean ~ (forall r. (boolean -> r) -> Expr r)
 
-type Bool<A> = {
+interface Bool<A> {
     value: boolean
     eq: (_: boolean) => A
     _tag: "Bool"
@@ -58,7 +58,7 @@ function Bool_y(b: boolean) {
 // --------------------------- Add<A> ---------------------------
 // (Functor Expr) => Expr number ~ (forall r. (number -> r) -> Expr r)
 
-type Add<A> = {
+interface Add<A> {
     en1: Expr<number>
     en2: Expr<number>
     eq: (_: number) => A
@@ -78,7 +78,7 @@ function Add_y(en1: Expr<number>, en2: Expr<number>) {
 // --------------------------- Mul<A> ---------------------------
 // (Functor Expr) => Expr number ~ (forall r. (number -> r) -> Expr r)
 
-type Mul<A> = {
+interface Mul<A> {
     en1: Expr<number>
     en2: Expr<number>
     eq: (_: number) => A
@@ -98,31 +98,31 @@ function Mul_y(en1: Expr<number>, en2: Expr<number>) {
 // --------------------------- Equal<A> ---------------------------
 // (Functor Expr) => Expr boolean ~ (forall r. (boolean -> r) -> Expr r)
 
-type EqualU<A, B> = {
-    eb1: Expr<B>
-    eb2: Expr<B>
+interface EqualU<A, X> {
+    ex1: Expr<X>
+    ex2: Expr<X>
     eq: (_: boolean) => A
 }
 
-type Equal<A> = {
+interface Equal<A> {
   // ∃B.Equal<A, B> ~ ∀R.((∃A.Equal<A, B>) => R) => R
   equal: <R>(_: <B>(_: EqualU<A, B>) => R) => R,
   _tag: "Eq"
 }
 
-// this constructor combines two Expr<B> into an encoded Expr<boolean> representing their equality
-function Equal_y<B>(eb1: Expr<B>, eb2: Expr<B>) {
+// this constructor combines two Expr<X> into an encoded Expr<boolean> representing their equality
+function Equal_y<X>(ex1: Expr<X>, ex2: Expr<X>) {
     return <R>(eq:(_: boolean) => R): Expr<R> => ({
         equal: cont => cont({
-          eb1,
-          eb2,
+          ex1,
+          ex2,
           eq,
         }),
         _tag: "Eq",
     } satisfies Equal<R>)
 }
 
-// ---------------- functor instance for Expr<A> ----------------
+// ---------------- Functor instance for Expr<A> ----------------
 
 // Functor instance for Expr<A>, mandatory for Yoneda encoding
 function fmap<A, B>(f: ( _: A) => B, ea: Expr<A>): Expr<B> {
@@ -140,7 +140,7 @@ function fmap<A, B>(f: ( _: A) => B, ea: Expr<A>): Expr<B> {
             return Mul_y(ea.en1, ea.en2)((_) => f(ea.eq(_)))
         }
         case "Eq": {
-            return ea.equal(ea => Equal_y(ea.eb1, ea.eb2)((_) => f(ea.eq(_))))
+            return ea.equal(ea => Equal_y(ea.ex1, ea.ex2)((_) => f(ea.eq(_))))
         }
     }
 }
@@ -212,7 +212,7 @@ function evalExpr<A>(ea: Expr<A>): A {
             return ea.eq(evalExpr(ea.en1) * evalExpr(ea.en2))
         }
         case "Eq": {
-          return ea.equal(ea => ea.eq(evalExpr(ea.eb1) === evalExpr(ea.eb2)))
+          return ea.equal(ea => ea.eq(evalExpr(ea.ex1) === evalExpr(ea.ex2)))
         }
     }
 }
--- examples/wtf.ts
+++ examples/wtf.ts
@@ -1,25 +1,180 @@
 export {}
 
 // data Wtf a where
-//   Nul :: Wtf a
-//   WtfInt :: Int -> Wtf Int
-//   WtfSimpleEx :: a -> Wtf a
-//   WtfComplexEx :: a -> b -> c -> Wtf (a, b)
+//   Nul :: Wtf a -- isomorphic to: forall a'. Wtf a'
+//   Wnt :: Int -> Wtf Int
+//   SimpleEx :: forall a. a -> Wtf a
+//   ComplexEx :: forall a b c. a -> b -> c -> Wtf (a, b)
 
 type Wtf<A> =
   | Nul<A>
+  | Wnt<A>
+  | SimpleEx<A>
+  | ComplexEx<A>
 
-  // --------------------------- Nul<A> ---------------------------
-// (Functor Wtf) => Wtf a ~ (forall r. (a -> r) -> Wtf r)
 
-type Nul<A> = {
-    eq: (_: A) => A
+// --------------------------- Nul<A> ---------------------------
+// (Functor Wtf) => Wtf a' ~ (forall r. (a' -> r) -> Wtf r)
+
+interface NulU<A, _A> {
+    eq: (_: _A) => A
+}
+
+interface Nul<A> {
+    nul: <R>(_: <_A>(_: NulU<A, _A>) => R) => R
     _tag: "Nul"
 }
 
-function Nul_y() {
-    return <R>(eq:(_: R) => R): Wtf<R> => ({
-        eq,
+function Nul_y<_A>() {
+    return <R>(eq:(_: _A) => R): Wtf<R> => ({
+        nul: cont => cont({
+          eq,
+        }),
         _tag: "Nul",
     } satisfies Nul<R>)
 }
+
+// --------------------------- Wnt<A> ---------------------------
+// (Functor Wtf) => Wtf number ~ (forall r. (number -> r) -> Wtf r)
+
+interface Wnt<A> {
+    value: number
+    eq: (_: number) => A
+    _tag: "Wnt"
+}
+
+// this constructor lifts a number into an encoded Wtf<number>
+function Wnt_y(n: number) {
+    return <R>(eq:(_: number) => R): Wtf<R> => ({
+        value: n,
+        eq,
+        _tag: "Wnt",
+    } satisfies Wnt<R>)
+}
+
+// ------------------------ SimpleEx<A> ------------------------
+// (Functor Wtf) => Wtf a ~ (forall r. (a -> r) -> Wtf r)
+
+interface SimpleExU<A, _A> {
+    _a: _A,
+    eq: (_: _A) => A
+}
+
+interface SimpleEx<A> {
+    simple: <R>(_: <_A>(_: SimpleExU<A, _A>) => R) => R
+    _tag: "SimpleEx"
+}
+
+function SimpleEx_y<_A>(_a: _A) {
+    return <R>(eq:(_: _A) => R): Wtf<R> => ({
+        simple: cont => cont({
+          _a,
+          eq,
+        }),
+        _tag: "SimpleEx",
+    } satisfies SimpleEx<R>)
+}
+
+// ---------------------- ComplexEx<A> ----------------------
+// (Functor Wtf) => Wtf (a, b) ~ (forall r. ((a, b) -> r) -> Wtf r)
+
+interface ComplexExU<A, _A, _B, _C> {
+    _a: _A,
+    _b: _B,
+    _c: _C,
+    eq: (_: [_A, _B]) => A
+}
+
+interface ComplexEx<A> {
+    complex: <R>(_: <_A, _B, _C>(_: ComplexExU<A, _A, _B, _C>) => R) => R
+    _tag: "ComplexEx"
+}
+
+function ComplexEx_y<_A, _B, _C>(_a: _A, _b: _B, _c: _C) {
+    return <R>(eq:(_: [_A, _B]) => R): Wtf<R> => ({
+        complex: cont => cont({
+          _a,
+          _b,
+          _c,
+          eq,
+        }),
+        _tag: "ComplexEx",
+    } satisfies ComplexEx<R>)
+}
+
+// ---------------- Functor instance for Wtf<A> ----------------
+
+// Functor instance for Wtf<A>, mandatory for Yoneda encoding
+function fmap<A, B>(f: (_: A) => B, w: Wtf<A>): Wtf<B> {
+    switch(w._tag) {
+        case "Nul": {
+          return w.nul(_ => Nul_y<B>()(_ => _))
+        }
+        case "Wnt": {
+          return Wnt_y(w.value)(_ => f(w.eq(_)))
+        }
+        case "SimpleEx": {
+          return w.simple(w => SimpleEx_y(f(w.eq(w._a)))(_ => _))
+        }
+        case "ComplexEx": {
+          return w.complex(w => ComplexEx_y(w._a, w._b, w._c)(_ => f(w.eq(_))))
+        }
+    }
+}
+
+// ---------------- Yoneda encoding & decoding ----------------
+
+// (Functor f) => f a -> (forall r. (a -> r) -> f r)
+// where f is fixed to Wtf for simplicity
+function to_yoneda<A>(decoded: Wtf<A>): <R>(eq: (_: A) => R) => Wtf<R> {
+    return eq => fmap(eq, decoded)
+}
+
+// (Functor f) => (forall r. (a -> r) -> f r) -> f a
+// where f is fixed to Wtf for simplicity
+function from_yoneda<A>(encoded: <R>(eq: (_: A) => R) => Wtf<R>): Wtf<A> {
+    return encoded(_ => _)
+}
+
+// ------------------- helpers -------------------
+
+function nul<A>(): Wtf<A> {
+    return from_yoneda(Nul_y<A>())
+}
+
+function wnt(n: number): Wtf<number> {
+    return from_yoneda(Wnt_y(n))
+}
+
+function simple<A>(_a: A): Wtf<A> {
+    return from_yoneda(SimpleEx_y(_a))
+}
+
+function complex<A, B, C>(a: A, b: B, c: C): Wtf<[A, B]> {
+    return from_yoneda(ComplexEx_y(a, b, c))
+}
+
+// ------------------- Eval -------------------
+
+function evalWtf<A>(w: Wtf<A>): A {
+  switch(w._tag) {
+    case "Nul": {
+      throw new Error("Nul")
+    }
+    case "Wnt": {
+      return w.eq(w.value)
+    }
+    case "SimpleEx": {
+      return w.simple(w => w.eq(w._a))
+    }
+    case "ComplexEx": {
+      return w.complex(w => w.eq([w._a, w._b]))
+    }
+  }
+}
+
+// examples
+
+const complexWtf = fmap(([s, n]) => `${s}${n}`, complex("a", 1, true))
+
+console.log(evalWtf(complexWtf))
